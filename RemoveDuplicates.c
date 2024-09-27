a8c52769d8be5075436da8a0cf2a80118d2203b4
ukarthikeycs23 <ukarthikey.cs23@bmsce.ac.in>
2024-09-27 15:34:49 +0530

    Create RemoveDuplicates.c

---
+++ RemoveDuplicates.c
@@ -0,0 +1,12 @@
+int removeDuplicates(int* nums, int numsSize) {
+    int j=0;
+    for(int i=0;i<numsSize;i++)
+    {
+        if(nums[i]!=nums[j])
+        {
+            j++;
+            nums[j]=nums[i];
+        }
+    }
+    return j+1;
+}
